0cad1d300971dd4054689ca89aa85cc72aa49cdc
Silmar <silmarjr2@gmail.com>
2023-07-20 15:32:52 -0300

    add animelist to routing

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,14 @@ const routes = [
     ],
   },
 
+  {
+    path: '/animelist',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/AnimeList.vue') },
+    ],
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
