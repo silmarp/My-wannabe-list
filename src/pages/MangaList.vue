ae4e9e2c164614ed832aeeca5da3237141a6ce40
Silmar <silmarjr2@gmail.com>
2023-07-20 16:24:17 -0300

    create mangalist and add to router

---
+++ src/pages/MangaList.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    LISTA DE MANGAS AQUI
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'MangaList',
+  data() {
+    return {
+      mangas: undefined,
+    };
+  },
+  methods: {
+    async getMangas() {
+      const url = 'https://api.jikan.moe/v4/manga';
+      const response = fetch(url);
+      this.mangas = await response.json().data;
+      console.log(this.mangas);
+    },
+  },
+};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -14,6 +14,13 @@ const routes = [
       { path: '', component: () => import('pages/AnimeList.vue') },
     ],
   },
+  {
+    path: '/mangalist',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/MangaList.vue') },
+    ],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
