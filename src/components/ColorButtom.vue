8610d22654f170ac3e5006a0aaf1557292bd5984
mRlFranchi <murilo.franchi@usp.br>
2023-07-21 15:13:49 -0300

    comit to change branch

--- src/components/ColorButtom.vue
+++ src/components/ColorButtom.vue
@@ -1,25 +1,22 @@
 <script setup>
 import { ref } from 'vue';
 
-const vIcon = 'star';
-const dColor = ref('grey'); // default color
-const aColor = ref('yellow'); // color on click
-// const vColor = ref(dColor.value);
-
-const dBtmColor = ref('white'); // button color
-const aBtmColor = ref('#FF0080'); // button color
-// const vBtmColor = ref(dBtmColor.value);
-
-const activated = ref(false);
-const vLabel = ref('label');
-const vStyle = ref(`background: ${dBtmColor.value}; color: ${aColor.value}`);
+props: {
+    vIcon: String;
+    dColor: String;
+    aColor: String;
+    dBtmColor: String;
+    aBtmColor: String;
+    vLabel: String;
+}
+const vStyle = ref(`background: ${dBtmColor}; color: ${aColor}`);
 
 function changeColor() {
   activated.value = !activated.value;
   if (activated.value) {
-    vStyle.value = ref(`background: ${aBtmColor.value}; color: ${aColor.value}`);
+    vStyle.value = ref(`background: ${aBtmColor}; color: ${aColor}`);
   } else {
-    vStyle.value = ref(`background: ${dBtmColor.value}; color: ${dColor.value}`);
+    vStyle.value = ref(`background: ${dBtmColor}; color: ${dColor}`);
   }
 }
 </script>
--- src/pages/AnimeView.vue
+++ src/pages/AnimeView.vue
@@ -1,8 +1,14 @@
+<script setup>
+import ColorButtom from 'src/components/ColorButtom.vue';
+</script>
 <template>
-    <q-page class="flex flex-center">
-      Informações sobre o animes
-    </q-page>
-  </template>
+  <q-page class="flex flex-center">
+    Informações sobre o animes
+    <ColorButtom dColor = 'grey' dBtmColor = 'white' aBtmColor = 'purple' aColor = 'yellow' vLabel = 'Favorite'/>
+    <ColorButtom/>
+    <ColorButtom/>
+  </q-page>
+</template>
 
 <script>
 export default {
