eec244adb55277d5bf968b4f60561bcd957072c7
mRlFranchi <murilo.franchi@usp.br>
2023-07-21 15:27:23 -0300

    made script like manga branch

--- src/pages/AnimeList.vue
+++ src/pages/AnimeList.vue
@@ -1,27 +1,3 @@
-<script setup>
-import { ref } from 'vue';
-
-const animes = ref([]);
-const selectedAnime = ref();
-const fetchingAnimes = ref(false);
-
-// https://jsonplaceholder.typicode.com/todos
-
-async function fetchAnimes(page) {
-  fetchingAnimes.value = true;
-
-  const fetcher = await fetch(`https://api.jikan.moe/v4/anime?page=${String(page)}`);
-  const data = await fetcher.json();
-  fetchingAnimes.value = false;
-
-  animes.value = data.data;
-
-  [selectedAnime.value] = animes.value;
-}
-
-fetchAnimes(1);
-</script>
-
 <template>
   <q-page class="column flex-left">
     <div class="q-pa-md" style="max-width: 350px;">
@@ -57,3 +33,26 @@ clickable v-ripple to="/animeview">
     </div>
   </q-page>
 </template>
+
+<script>
+
+export default {
+  name: 'AnimeList',
+  data() {
+    return {
+      animes: undefined,
+    };
+  },
+  methods: {
+    async getAnimes(page) {
+      const url = `https://api.jikan.moe/v4/anime?page=${String(page)}`;
+      const response = await fetch(url);
+      const jsonObj = await response.json();
+      this.animes = jsonObj.data;
+    },
+  },
+  beforeMount() {
+    this.getAnimes(1);
+  },
+};
+</script>
